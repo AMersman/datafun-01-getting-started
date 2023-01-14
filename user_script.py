3023ee976da216738120819eec143302d91ad590
AMersman <91644580+AMersman@users.noreply.github.com>
2023-01-14 09:20:40 -0600

    Final Module 1 Project

--- user_script.py
+++ user_script.py
@@ -1,3 +1,28 @@
 """
 Complete the script.
 """
+
+movie = input("Pick a movie from the following. Iron Man, Captain American, Thor")
+
+if movie == "Iron Man":
+    print("Iron Man stars Robert Downey Jr. and made $585 Million at the Box Office")
+elif movie == "Captian America":
+    print("Captain America stars Chris Evans and made $714 Million at the Box Office")
+elif movie == "Thor":
+    print("Thor stars Chris Hemsworth and made $449 Million at the Box Office")
+else:
+    print("That wasn't an option")
+
+profits = [585, 714, 449]
+
+minimum = min(profits)
+maximum = max(profits)
+average = sum(profits)/3
+total = sum(profits)
+print("The profit range was {} to {} million dollars.".format(minimum, maximum))
+
+
+
+
+
+
