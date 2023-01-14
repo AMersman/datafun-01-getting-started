45eb0060cef9b42e7a4dcbc363b54e7fafa5862a
AMersman <91644580+AMersman@users.noreply.github.com>
2023-01-14 09:44:15 -0600

    initial submission with bonus

--- xtra_p1.py
+++ xtra_p1.py
@@ -1,18 +1,18 @@
 """
 Optional bonus. See course site for details.
-
+Ashley Mersman, gamebot using logic, 1/14/2023
 """
 
 import random
 
 # Change the name below to a name of your choice
 
-name = "GameBot"
+name = "Nature's Choice"
 
 # Fix the code below to print the name using an f-string
 
 print()
-print("Hello, I'm {name}, your gamebot.")
+print(f"Hello, I'm {name}, your gamebot.")
 print("Let's play an animal guessing game!")
 print("There are 3 animals: wolf, eagle, snake (a Python of course).")
 print("The wolf scares the eagle.")
@@ -26,7 +26,7 @@ print()
 # enter wolf, eagle, or snake.
 # Hint: use the input() function
 
-user_choice = "wolf"
+user_choice = input('Choose wolf, eagle, or snake.')
 
 # Now the bot will pick one
 buddy_choice = random.choice(["wolf", "eagle", "snake"])
@@ -44,6 +44,21 @@ print()
 # In Python, indentation is important!
 if user_choice == buddy_choice:
     print("We tied!")
+if user_choice == "wolf":
+    if buddy_choice == "eagle":
+        print("You win! Wolf scares eagle.")
+    elif buddy_choice == 'snake':
+        print("You lose. Snake bites wolf.")
+if user_choice == "eagle":
+    if buddy_choice == "wolf":
+        print("You lose! Wolf scares eagle.")
+    elif buddy_choice == 'snake':
+        print("You win. Eagle grabs snake.")
+if user_choice == "snake":
+    if buddy_choice == "wolf":
+        print("You win! Snake bites wolf.")
+    elif buddy_choice == 'eagle':
+        print("You lose. Eagle grabs snake.")
 
 
 # When you finish,
@@ -54,8 +69,10 @@ if user_choice == buddy_choice:
 # docstring comment below.
 # --------------------------------------------------------------------
 """
+You said wolf.
+I said snake.
 
-
-
-
+You lose. Snake bites wolf.
 """
+
+"""Ashley Mersman, 1/14/2023, Game-bot"""
